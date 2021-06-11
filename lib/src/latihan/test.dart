eca2f7e312909d96835c9a9cfaa3adbd370ae266
Ahmad Saifullah <ahsai001@gmail.com>
2021-06-11 22:21:58 +0700

    update

--- lib/src/latihan/test.dart
+++ lib/src/latihan/test.dart
@@ -8,6 +8,12 @@ class TestPage extends StatefulWidget {
 }
 
 class _TestPageState extends State<TestPage> {
+  bool _switchValue = false;
+  bool _checkboxValue1 = false;
+  bool _checkboxValue2 = false;
+  bool _checkboxValue3 = false;
+  int? _radioValue = 1;
+  String? _dropdownValue = "IOS";
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -15,9 +21,7 @@ class _TestPageState extends State<TestPage> {
         title: Text("Test Page"),
       ),
       body: SafeArea(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.start,
-          crossAxisAlignment: CrossAxisAlignment.start,
+        child: ListView(
           children: [
             Text("Hello Text"),
             TextField(
@@ -26,46 +30,104 @@ class _TestPageState extends State<TestPage> {
               ),
               toolbarOptions: ToolbarOptions(copy: true, paste: true),
             ),
-            // SwitchListTile(
-            //     title: Text("OK?"), value: true, onChanged: (value) {}),
-            // Row(
-            //   children: [
-            //     CheckboxListTile(
-            //         title: Text("Android"), value: false, onChanged: (value) {}),
-            //     CheckboxListTile(
-            //         title: Text("IOS"), value: false, onChanged: (value) {}),
-            //     CheckboxListTile(
-            //         title: Text("Windows"), value: false, onChanged: (value) {})
-            //   ],
-            // ),
-            // Row(
-            //   children: [
-            //     RadioListTile(
-            //         title: Text("Android"),
-            //         value: false,
-            //         onChanged: (value) {},
-            //         groupValue: true),
-            //     RadioListTile(
-            //         title: Text("IOS"),
-            //         value: false,
-            //         onChanged: (value) {},
-            //         groupValue: true),
-            //     RadioListTile(
-            //       title: Text("Windows"),
-            //       value: false,
-            //       onChanged: (value) {},
-            //       groupValue: true,
-            //     )
-            //   ],
-            // ),
-            // DropdownButton<String>(
-            //   items: [
-            //     DropdownMenuItem(child: Text("Android")),
-            //     DropdownMenuItem(child: Text("IOS")),
-            //     DropdownMenuItem(child: Text("Windows")),
-            //   ],
-            // ),
-            // CircularProgressIndicator(),
+            SwitchListTile(
+                title: Text("Title"),
+                subtitle: Text("Subtitle"),
+                secondary: Text("secondary"),
+                controlAffinity: ListTileControlAffinity.leading,
+                value: _switchValue,
+                onChanged: (value) {
+                  setState(() {
+                    _switchValue = value;
+                  });
+                }),
+            CheckboxListTile(
+                title: Text("Title"),
+                subtitle: Text("Subtitle"),
+                secondary: Text("secondary"),
+                controlAffinity: ListTileControlAffinity.leading,
+                value: _checkboxValue1,
+                onChanged: (value) {
+                  setState(() {
+                    _checkboxValue1 = value!;
+                  });
+                }),
+            CheckboxListTile(
+                title: Text("Title"),
+                subtitle: Text("Subtitle"),
+                secondary: Text("secondary"),
+                controlAffinity: ListTileControlAffinity.leading,
+                value: _checkboxValue2,
+                onChanged: (value) {
+                  setState(() {
+                    _checkboxValue2 = value!;
+                  });
+                }),
+            CheckboxListTile(
+                title: Text("Title"),
+                subtitle: Text("Subtitle"),
+                secondary: Text("secondary"),
+                controlAffinity: ListTileControlAffinity.leading,
+                value: _checkboxValue3,
+                onChanged: (value) {
+                  setState(() {
+                    _checkboxValue3 = value!;
+                  });
+                }),
+            RadioListTile(
+                title: Text("Title"),
+                subtitle: Text("Subtitle"),
+                secondary: Text("secondary"),
+                controlAffinity: ListTileControlAffinity.leading,
+                value: 1,
+                onChanged: (int? value) {
+                  setState(() {
+                    _radioValue = value!;
+                  });
+                },
+                groupValue: _radioValue),
+            RadioListTile(
+                title: Text("Title"),
+                subtitle: Text("Subtitle"),
+                secondary: Text("secondary"),
+                controlAffinity: ListTileControlAffinity.leading,
+                value: 2,
+                onChanged: (int? value) {
+                  setState(() {
+                    _radioValue = value!;
+                  });
+                },
+                groupValue: _radioValue),
+            RadioListTile(
+              title: Text("Title"),
+              subtitle: Text("Subtitle"),
+              secondary: Text("secondary"),
+              controlAffinity: ListTileControlAffinity.leading,
+              value: 3,
+              onChanged: (int? value) {
+                setState(() {
+                  _radioValue = value!;
+                });
+              },
+              groupValue: _radioValue,
+            ),
+            DropdownButton<String>(
+              items: [
+                DropdownMenuItem(value: "Android", child: Text("Android")),
+                DropdownMenuItem(value: "IOS", child: Text("IOS")),
+                DropdownMenuItem(value: "Windows", child: Text("Windows")),
+              ],
+              value: _dropdownValue,
+              onChanged: (value) {
+                setState(() {
+                  _dropdownValue = value;
+                });
+              },
+            ),
+            UnconstrainedBox(
+              child: CircularProgressIndicator(),
+              alignment: Alignment.centerLeft,
+            ),
           ],
         ),
       ),
